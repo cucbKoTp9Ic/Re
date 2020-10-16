456622f131346ddac17559057764865c5e104f1e
cucbKoTp9Ic <64649635+cucbKoTp9Ic@users.noreply.github.com>
2020-10-16 17:01:25 +0500

    2.0.0

--- js/serv.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Привет чувырла');
-});
-
-server.listen(135, '127.0.0.1');
-console.log('Порт 3000 ёпта')
